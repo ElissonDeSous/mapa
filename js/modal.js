b6b31757e4ebf7c3413924d611b89a4f898efaab
Elisson De Sous <elissondsousa@gmail.com>
2025-07-05 14:57:17 -0300

    alterações na estrutura de tudo

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+let modal = document.getElementById('modal')
+function abrir(){  
+    modal.style.display = 'flex'
+
+}
+
+function fechar(){
+     modal.style.display = "none"
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-
 function validacao() {
-  
   let nome = document.getElementById("nome");
   let email = document.getElementById("email");
 
@@ -8,20 +6,15 @@ function validacao() {
   let name = String(nome.value);
   let Email = String(email.value);
 
- 
- 
- if (name == "" || Email == "") {
-    nome.style.border = '1px solid red'
-    email.style.border = '1px solid red'
+  if (name == "" || Email == "") {
+    nome.style.border = "1px solid red";
+    email.style.border = "1px solid red";
     resposta.style.color = "red";
     resposta.innerHTML = "preencha todos os campos";
   } else {
-      nome.style.border = '1px solid green'
-      email.style.border = '1px solid green'
+    nome.style.border = "1px solid green";
+    email.style.border = "1px solid green";
     resposta.style.color = "green";
     resposta.innerHTML = "Mensagem enviada com sucesso";
-  } 
-  name.reset()
-  email.reset()
-  
+  }
 }
